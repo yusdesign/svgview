ac4a1acf59e0bd75a0001238b25293b2328ba87a
yusdesign <swiftcase@gmail.com>
2026-06-21 08:11:30 +0800

    🌇 web fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/svgview/',
   server: {
     host: true,
-    port: 3000
+    port: 3000,
+    open: true
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['@capacitor/core', '@capacitor/filesystem']
+        }
+      }
+    }
   },
   plugins: [],
   optimizeDeps: {
